36eafd4d487a0f9f157a9755386233401e6bb469
Wuirlen <wuirlen.azevedo@gmail.com>
2021-06-04 15:51:09 -0400

    Projeto Plataforma EAD

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <title>EAD - Tuti Academy</title>
+    <link rel="shortcut icon" href="./img/graduation-hat.ico" >
+<style>
+body{
+    background-color: #15321a;
+}
+@font-face {
+     font-family: Roboto;
+     src: url('./Roboto/Roboto-Light.ttf');
+}
+p{
+    font-family: Roboto;
+}
+</style>
+
+</head>
+<body>
+
+<div class="card-deck center-block" style ="width: 80%; margin: 100px auto; float: none; margin-bottom: 100px;">
+  <div class="card shadow-lg p-3 mb-5 bg-white rounded center-block" >
+    <img class="card-img-top rounded mx-auto d-block" src="./img/instructor.png" alt="Imagem de capa do card" style ="width: 200px; height: 200px;">
+    <div class="card-body text-center">
+      <h5 class="card-title">Área do instrutor</h5>
+      <p class="card-text">Os melhores instrutores lecionam aqui. Oferecemos as ferramentas e habilidades para ensinar o que você ama.</p>
+    </div>
+    <div class="card-footer">
+    <button type="button" class="btn btn-success btn-lg btn-block" onclick="window.location.href='./admin/'" >EAD - Admin</button>
+    </div>
+  </div>
+  <div class="card shadow-lg p-3 mb-5 bg-white rounded center-block" >
+    <img class="card-img-top rounded mx-auto d-block" src="./img/graduated.png" alt="Imagem de capa do card" style ="width: 200px; height: 200px;">
+    <div class="card-body text-center">
+      <h5 class="card-title ">Aprenda no seu tempo</h5>
+      <p class="card-text">Estude qualquer tema, a qualquer hora. Explore milhares de cursos.</p>
+    </div>
+    <div class="card-footer">
+    <button type="button" class="btn btn-success btn-lg btn-block" onclick="window.location.href='./ead/'">EAD - Campus</button>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>    
+</body>
+</html>
